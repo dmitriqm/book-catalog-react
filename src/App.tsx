74f672f8d3ec89c9873d1e898132bf99181bc6d2
1dmitriqm@gmail.com <1dmitriqm@gmail.com>
2022-05-01 22:41:19 +0500

    finish desktop navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import Catalog from "./pages/Catalog";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="*" element={<h1>Not found</h1>}></Route>
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="*" element={<></>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,35 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+const navs = [
+  { title: "Поиск", to: "/search" },
+  { title: "Книги", to: "/books" },
+  { title: "Каталог", to: "/catalog" },
+];
 
 const Navbar = () => {
   return (
     <nav className="flex items-center border-b h-20 border-slate-200 bg-white">
+      <div className="px-4">
+        <NavLink to="/" className={""}>
+          <img className="h-14" src={logo} alt=""></img>
+        </NavLink>
+      </div>
+
+      <div className="flex h-full">
+        {navs.map(({ title, to }, idx) => (
+          <NavLink
+            key={idx}
+            to={to}
+            className={({ isActive }) =>
+              "h-full flex mx-1 px-3 items-center justify-center  hover:border-b-2 box-content hover:border-amber-500 hover:text-amber-600 text-xl" +
+              `${isActive ? " border-amber-500 text-amber-600 border-b-2" : ""}`
+            }
+          >
+            {title}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   );
 };
