f787976c03d46f8d2186a03caf4e9c174e851d23
1dmitriqm@gmail.com <1dmitriqm@gmail.com>
2022-05-03 12:13:33 +0500

    redux append

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDHd7M1sYTZowoydWRxLu1G56XH4jSnarQ",
-  authDomain: "book-catalog-ff906.firebaseapp.com",
-  projectId: "book-catalog-ff906",
-  storageBucket: "book-catalog-ff906.appspot.com",
-  messagingSenderId: "663097301726",
-  appId: "1:663097301726:web:854693a95a28f78608404f",
-};
---
+++ src/store/layout/layout.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface LayoutState {
+  navmenu: {
+    isOpen: boolean;
+  };
+}
+
+const initialState = {
+  navmenu: {
+    isOpen: false,
+  },
+} as LayoutState;
+
+const layoutSlice = createSlice({
+  name: "layout",
+  initialState,
+  reducers: {
+    toggleMenu(state) {
+      state.navmenu.isOpen = !state.navmenu.isOpen;
+    },
+  },
+});
+
+export const { toggleMenu } = layoutSlice.actions;
+export default layoutSlice.reducer;
