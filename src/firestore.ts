439de4913c9c6f29c1d1c5e9e6c86b02cff2771a
1dmitriqm@gmail.com <1dmitriqm@gmail.com>
2022-06-10 11:12:48 +0500

    new cfg

--- src/firestore.ts
+++ src/firestore.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const config = {
-  apiKey: "AIzaSyDHd7M1sYTZowoydWRxLu1G56XH4jSnarQ",
-  authDomain: "book-catalog-ff906.firebaseapp.com",
-  projectId: "book-catalog-ff906",
-  storageBucket: "book-catalog-ff906.appspot.com",
-  messagingSenderId: "663097301726",
-  appId: "1:663097301726:web:854693a95a28f78608404f",
+  apiKey: "AIzaSyAXTJW3SnCI3nT0xN8k8PMq92pz0iwMkU4",
+  authDomain: "book-catalog-2.firebaseapp.com",
+  projectId: "book-catalog-2",
+  storageBucket: "book-catalog-2.appspot.com",
+  messagingSenderId: "370771363078",
+  appId: "1:370771363078:web:a6b73cf869d53df258ca87",
 }
 
 export const firestoreApp = initializeApp(config)
